5363e0ccd777e8c180c2fc3a1bca2840adeb7a86
frinhanifrinhani <frinhani.frinhani@gmail.com>
2017-11-13 14:49:10 -0200

    Teste de conexão
    Teste de conexão

--- src/conexao/JDBCExemplo.java
+++ src/conexao/JDBCExemplo.java
@@ -7,15 +7,19 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
 
 public class JDBCExemplo {
-	/*public static void main(String[] args) throws SQLException {
-        Connection conexao = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/java_web_cae","root","0777");
-        System.out.println("Conectado!");
-        conexao.close();
-    }*/
-	
+	/*
+	 * public static void main(String[] args) throws SQLException { Connection
+	 * conexao = (Connection)
+	 * DriverManager.getConnection("jdbc:mysql://localhost/java_web_cae","root",
+	 * "0777"); System.out.println("Conectado!"); conexao.close(); }
+	 */
+
 	public static void main(String[] args) {
-		ConnectionFactory conexao = new ConnectionFactory();
-		System.out.println(conexao.getConnection());
-		
-	}
+		ConnectionFactory conexao = (ConnectionFactory) new ConnectionFactory();
+			//System.out.println(conexao.getConnection());
+			if(!(conexao == null)) {
+				System.out.println("sim");
+			}
+		}
+
 }
